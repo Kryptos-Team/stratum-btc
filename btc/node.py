6dec387d8cd4b2e4bed9373bc8613c50e337ea41
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2021-01-04 00:10:33 +0530

    Initial commit for node

---
+++ btc/node.py
@@ -0,0 +1,10 @@
+import struct
+import socket
+import binascii
+import time
+import sys
+import random
+from io import StringIO
+from Crypto.Hash import SHA256
+from twisted.internet.protocol import Protocol
+
