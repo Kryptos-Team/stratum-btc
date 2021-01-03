4611df7b12df48586272e16684d8fa11c3d5909b
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2021-01-04 00:12:27 +0530

    Add BitcoinAddress to __init__.py

--- btc/__init__.py
+++ btc/__init__.py
@@ -1,8 +1,10 @@
-from .rpc import BitcoinRPC
-from .service import MiningService
-from .subscription import MiningSubscription
 from twisted.internet import defer
 import time
+from logzero import logger
+from stratum.rpc import Rpc
+from .service import MiningService
+from .subscription import MiningSubscription
+from .address import BitcoinAddress
 
 
 @defer.inlineCallbacks
@@ -13,6 +15,19 @@ def setup(on_startup):
     (yield Interfaces.share_manager.on_load)
     (yield Interfaces.worker_manager.on_load)
 
-    bitcoin_rpc = BitcoinRPC(host="18.179.58.207", port=18444, username="kryptos", password="x")
+    bitcoin_rpc = Rpc(url="http://localhost")
+
+    logger.info("Waiting for bitcoin rpc")
+
+    while True:
+        network_info = (yield bitcoin_rpc.getnetworkinfo())
+        if isinstance(network_info, dict):
+            break
+        else:
+            logger.warning("sleeping...")
+            time.sleep(1)
+
+    pool_address = BitcoinAddress(rpc=bitcoin_rpc, address="xxx")
+    (yield pool_address.on_load)
 
     on_startup.callback(True)
